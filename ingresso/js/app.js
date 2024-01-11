667d76b6a4f458db10d5d79e7430bc78974fadb4
Talita Gonçalves Barroso <talita.t.b001@gmail.com>
2024-01-11 11:25:26 -0300

    Projetinhos de lógica de programação em JS

---
+++ ingresso/js/app.js
@@ -0,0 +1,51 @@
+
+function comprar(){
+    //recuperar o tipo e quantidade do ingresso
+    let tipo = document.getElementById('tipo-ingresso').value;
+    let qtd = parseInt(document.getElementById('qtd').value);
+
+    if(tipo == 'pista'){
+        comprarPista(qtd);
+    }else if(tipo == 'superior' ){
+        comprarSuperior(qtd)
+    }else{
+        comprarInferior(qtd)
+    }
+    document.getElementById('qtd').value = '';
+}
+
+function comprarPista(qtd){
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+
+    if( qtd > qtdPista){
+        alert('Quantidade de ingressos indisponível para tipo Pista!');
+    }else{
+        qtdPista -= qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!!')
+    }
+}
+
+function comprarSuperior(qtd){
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+
+    if( qtd > qtdSuperior){
+        alert('Quantidade de ingressos indisponível para tipo Cadeira Superior!');
+    }else{
+        qtdSuperior -= qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!!')
+    }
+}
+
+function comprarInferior(qtd){
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    if( qtd > qtdInferior){
+        alert('Quantidade de ingressos indisponível para tipo Cadeira Inferior!');
+    }else{
+        qtdInferior -= qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso!!')
+    }
+}
